812791509017d2a54a7e05a74634a0d41a6fa1fd
sclaiborne <scott@loupe.team>
2021-03-25 07:39:16 -0700

    Make ws protocol configurable
    This allows us to use wss (for future use and prototyping)

--- webhmi.js
+++ webhmi.js
@@ -583,7 +583,7 @@ WEBHMI.Machine = function (options) {
 
 		function openWebSocket() {
 
-			ws = new WebSocket("ws://" + settings.ipAddress + ':' + settings.port);
+			ws = new WebSocket(settings.protocol + "://" + settings.ipAddress + ':' + settings.port);
 
 			ws.onopen = function () {
 				clearTimeout(read.timeout);

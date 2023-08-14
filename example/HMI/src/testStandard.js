c61bf56a681cfaf72a083d77f2720ca5a5aec6e8
Bishop Varnell <bishop@loupe.team>
2023-08-14 09:31:39 -0600

    adapt example for tmplit rename

--- example/HMI/src/testStandard.js
+++ example/HMI/src/testStandard.js
@@ -1,8 +1,6 @@
 machine = new WEBHMI.Machine({
   port: 8000,
   ipAddress: '127.0.0.1',
-  maxReconnectCount: 5000,
-});
-
-
-setInterval(WEBHMI.updateHMI, 30)
+  maxReconnectCount: 5000
+});  
+setInterval(WEBHMI.updateHMI,30)
